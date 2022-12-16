3708449d405096883a304c227e15d96376e034d3
Camilo716 <camilo.gonzalez.col@gmail.com>
2022-12-15 19:17:02 -0500

    Ejercicio 1

---
+++ C#/Ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program{
+    
+    public static void Main(string[] args) {
+        System.Console.WriteLine(getCurrentTime());
+        System.Console.WriteLine(getName());
+    }
+
+    public static String getName(){
+        System.Console.WriteLine("Please Write your name");
+        String name = Console.ReadLine();
+        return name;
+    }
+
+    public static String getCurrentTime() {
+        string currentTime = DateTime.Now.ToString("hh:mm:ss tt");
+        return currentTime;
+    }
+}
+
+
